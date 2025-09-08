f3bbc21b29d0014b76be5b2ab6f9c71524e362a5
Hunter <120475102+GigaGitCoder@users.noreply.github.com>
2025-09-08 17:56:05 +0300

    modified:   Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "common.h"
 
 using namespace std;
@@ -7,7 +8,7 @@ int main()
 {
     setupLocale();
     setlocale(LC_ALL, "Russian");
-    cout << "Penis, Пенис";
+    cout << "Test";
 
     return 0;
 }
